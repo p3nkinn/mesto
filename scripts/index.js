4dfd19fe23238dbc6b6acf1f4651833966e87ec4
Sergey Penkin <p3nkinn@gmail.com>
2022-03-16 12:51:53 +0300

    js fixed code

--- scripts/index.js
+++ scripts/index.js
@@ -11,21 +11,36 @@ const popup = document.querySelector('.popup'),
 
 function popupOpen() {
   popup.classList.add('popup_opened');
+  document.addEventListener('keyup', onDocumentKeyUp);
 }
 function popupClosed() {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keyup', onDocumentKeyUp);
 }
 
 nameInput.value = nameForm.innerHTML;
 jobInput.value = jobForm.innerHTML;
 
+function onDocumentKeyUp (e) {
+  if (e.key === ESC_KEY) {
+    popupClosed();
+  }
+  else if (e.key === ENT_KEY) {
+    popupClosed();
+  }
+}
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameForm.textContent = nameInput.value;
     jobForm.textContent = jobInput.value;
-    popupClosed();
+    document.addEventListener('keyup', onDocumentKeyUp);
+
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 btnEdit.addEventListener('click', popupOpen);
 popupClose.addEventListener('click', popupClosed);
+document.addEventListener('keyup', onDocumentKeyUp);
+const ESC_KEY = "Escape";
+const ENT_KEY = "Enter";
