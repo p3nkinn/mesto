b4fefc5a4eb09140ddaff6af137025b96ad3fea4
Sergey Penkin <p3nkinn@gmail.com>
2022-03-20 13:13:36 +0300

    fixed html style js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,31 @@
 "use strict"
-
+// Находим элементы попапа и формы
 const popup = document.querySelector('.popup'),
       popupClose = popup.querySelector('.popup__close'),
       formElement = popup.querySelector('.popup__form'),
-      nameInput = formElement.querySelector('.popup__name'),
-      jobInput = formElement.querySelector('.popup__jobname'),
+      nameInput = formElement.querySelector('.popup__input_type_username'),
+      jobInput = formElement.querySelector('.popup__input_type_userjob'),
       btnEdit = document.querySelector('.profile__edit-button'),
-      nameForm = document.querySelector('.profile__title'),
-      jobForm = document.querySelector('.profile__subtitle');
+      profileName = document.querySelector('.profile__title'),
+      jobName = document.querySelector('.profile__subtitle');
 
-function popupOpen() {
+function openPopup() {
   popup.classList.add('popup_opened');
-  document.addEventListener('keyup', onDocumentKeyUp);
+  nameInput.value = profileName.textContent;
+  jobInput.value = jobName.textContent;
 }
-function popupClosed() {
+
+function closePopup() {
   popup.classList.remove('popup_opened');
-  document.removeEventListener('keyup', onDocumentKeyUp);
-}
-
-nameInput.value = nameForm.innerHTML;
-jobInput.value = jobForm.innerHTML;
-
-function onDocumentKeyUp (e) {
-  if (e.key === ESC_KEY) {
-    popupClosed();
-  }
-  else if (e.key === ENT_KEY) {
-    popupClosed();
-  }
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    nameForm.textContent = nameInput.value;
-    jobForm.textContent = jobInput.value;
-    document.addEventListener('keyup', onDocumentKeyUp);
-    popupClosed();
+    profileName.textContent = nameInput.value;
+    jobName.textContent = jobInput.value;
+    closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-btnEdit.addEventListener('click', popupOpen);
-popupClose.addEventListener('click', popupClosed);
-document.addEventListener('keyup', onDocumentKeyUp);
-const ESC_KEY = "Escape";
-const ENT_KEY = "Enter";
+btnEdit.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
