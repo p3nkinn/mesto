10b29ef1367d312313d422ca74f1bcb268035ee7
Sergey Penkin <p3nkinn@gmail.com>
2022-03-28 13:12:19 +0300

    change bem modif

--- scripts/index.js
+++ scripts/index.js
@@ -10,12 +10,12 @@ const popup = document.querySelector('.popup'),
       profileName = document.querySelector('.profile__title'),
       jobName = document.querySelector('.profile__subtitle'),
     // Форма с добавлением картинки и описания
-      newPlaces = document.querySelector('.popup__new-places'),
+      newPlaces = document.querySelector('.popup__newplaces'),
       newPlacesForm = newPlaces.querySelector('.popup__new-form'),
       btnAdd = document.querySelector('.profile__add-button'),
       titleNameForm = document.querySelector('.popup__input_type_title'),
       linkNameForm = document.querySelector('.popup__input_type_link'),
-      formImage = document.querySelector('.popup__open-image'),
+      formImage = document.querySelector('.popup__openimg'),
       addImage = formImage.querySelector('.popup__image'),
       addSubtitle = formImage.querySelector('.popup__subtitle');
 
